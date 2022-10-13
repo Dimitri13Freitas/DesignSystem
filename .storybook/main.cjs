fe48bb5b5cf6161d343eafdc19f3449e498dbb5a
Dimitri Freitas <freitasdimitri7@gmail.com>
2022-10-12 21:00:53 -0300

    Deploy

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if(config === 'PRODUCTION') {
+      config.base = '/DesignSystem/'
+    }
+    return config
   }
 }
